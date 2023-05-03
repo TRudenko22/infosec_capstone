f026db7d12a38278e46df95f1af8a02e100289d8
TRudenko22 <timothy.rudenkom@gmail.com>
2023-05-03 14:40:31 -0400

    added functionality to script

--- infrastructure/scripts/run.sh
+++ infrastructure/scripts/run.sh
@@ -2,11 +2,29 @@
 
 cd ../ctf_platform/
 
-terraform apply -auto-approve
+terraform apply -auto-approve >/dev/null
 wait $!
 
-echo $(terraform output)
+terraform output
+IP1=$(echo $(terraform output) \
+  | awk '{print $5}' \
+  | tr -d '"' \
+  | tr -d ","  )
 
 
-#ssh-copy-id -i ~/.ssh/linode
+IP2=$(echo $(terraform output) \
+  | awk '{print $4}' \
+  | tr -d '"' \
+  | tr -d ","  )
 
+if echo "$IP1" | grep -q "192.168"; then
+  PRIVATE_IP="$IP2"
+else
+  PRIVATE_IP="$IP1"
+fi
+
+echo "$PRIVATE_IP"
+ssh-copy-id -i ~/.ssh/linode root@"$PRIVATE_IP" 
+scp ../scripts/init.sh root@"$PRIVATE_IP":/init.sh 
+
+#ssh root@"$PRIVATE_IP" 
