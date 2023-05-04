28acf2ca9a0d4758426e3de6bc061e5891fc7250
TRudenko22 <timothy.rudenkom@gmail.com>
2023-05-03 21:23:17 -0400

    added variables tf to .gitignore

--- infrastructure/ctf_platform/variables.tf
+++ infrastructure/ctf_platform/variables.tf
@@ -1,7 +1,6 @@
 variable "access_token" {
     description = "Token provided by linode for access to API"
-    #default = "null"
-    default = "5a80a301db73be12c43968636b4cce5596c5c7b431c57d57a05c949c249270ca"
+    default = "null"
 }
 
 variable "root_pass" {
