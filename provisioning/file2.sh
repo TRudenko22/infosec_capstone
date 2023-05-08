a3f82af9a4ac3dae46fc493853e83649a361e4c0
piCounter <mike.docs2@gmail.com>
2023-05-08 11:43:59 -0400

    minor changes to vm config files

--- provisioning/file2.sh
+++ provisioning/file2.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
-# get file from gh and startup CTFd
+# run CTFd inside docker container
 
 # update
 apt update -y && echo -e "\nPackages updated\n"
-apt upgrade  -y && echo "\nPackages upgraded\n"
+apt upgrade  -y && echo -e "\nPackages upgraded\n"
 apt install git docker docker-compose -y 
 
 cd ${HOME}
-
-#git clone https://github.com/TRudenko22/infosec_capstone.git
 
 # CTFd
 git clone https://github.com/CTFd/CTFd.git
